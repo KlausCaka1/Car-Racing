234d9f4bdb566022cfe423aee4b26a8161867de8
klaus <klauscaka@fshnstudent.info>
2025-12-02 23:51:10 +0100

    Putting different levels

--- pythonProject/main.py
+++ pythonProject/main.py
@@ -1,33 +1,36 @@
+import os
+import re
+
 import pygame
-import time
 import math
 from utils import scaleImage, blit_rotate_img
 import neat
-import glob, os
 
 TRACK = scaleImage(pygame.image.load("imgs/track2.png"), 1)
 
 TRACK_BOARD = scaleImage(pygame.image.load("imgs/track-border.png"), 1)
 TRACK_BOARD_MASK = pygame.mask.from_surface(TRACK_BOARD)
 
-FINISH_LINE = scaleImage(pygame.image.load("imgs/finish.png"), 1.2)
+FINISH_LINE = scaleImage(pygame.image.load("imgs/finish.png"), 1)
+FINISH_LINE =  pygame.transform.scale(FINISH_LINE, (100, 116))
+FINISH_LINE = pygame.transform.rotate(FINISH_LINE, 180)
 FINISH_LINE_MASK = pygame.mask.from_surface(FINISH_LINE)
-FINISH_LINE_POS = (200, 700)
+FINISH_LINE_POS = [770, 878]
 
-RED_CAR = scaleImage(pygame.image.load("imgs/red-car.png"), 0.5)
-GREEN_CAR = scaleImage(pygame.image.load("imgs/green-car.png"), 0.5)
 
 WIDTH, HEIGHT = TRACK.get_width(), TRACK.get_height()
 WIN = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("RACE GAME")
 
+TRACKS = ['./imgs/track.png', './imgs/track1.png', './imgs/track2.png']
+global INDEX_TRACK
+
 FPS = 60
 generation = 1
-BORDER_COLOR = (255, 255, 255, 255) # Color To Crash on Hit
+BORDER_COLOR = (255, 255, 255, 255)  # Color To Crash on Hit
 
-
-CAR_SIZE_X = 60
-CAR_SIZE_Y = 60
+CAR_SIZE_X = 40
+CAR_SIZE_Y = 40
 
 
 # Color To Crash on Hit
@@ -38,13 +41,12 @@ class AbstractCar:
 
         self.sprite = pygame.image.load('./imgs/car.png').convert()  # Convert Speeds Up A Lot
         self.sprite = pygame.transform.scale(self.sprite, (CAR_SIZE_X, CAR_SIZE_Y))
+        self.mask = pygame.mask.from_surface(self.sprite)
         self.rotated_sprite = self.sprite
 
-        # self.position = [690, 740] # Starting Position
-        self.position = [830, 920]  # Starting Position
+        self.position = [860, 920]  # Starting Position
         self.angle = 0
         self.speed = 0
-
 
         self.speed_set = False  # Flag For Default Speed Later on
 
@@ -58,28 +60,8 @@ class AbstractCar:
         self.distance = 0  # Distance Driven
         self.time = 0  # Time Passed
 
-
-        self.checkpoint_hit = False
-
-    def rotate(self, left=False, right=False):
-        if left:
-            self.angle += self.rotation_vel
-        elif right:
-            self.angle -= self.rotation_vel
-
-
     def isAlive(self):
         return self.alive
-
-    def move(self):
-        radius = math.radians(self.angle)
-        veritcal = math.cos(radius) * self.vel
-        horisontal = math.sin(radius) * self.vel
-
-        self.x -= horisontal
-        self.y -= veritcal
-
-        self.center = [int(self.x + CAR_SIZE_X/2), int(self.y + CAR_SIZE_Y/2)]
 
     def draw(self, screen):
         screen.blit(self.rotated_sprite, self.position)  # Draw Sprite
@@ -91,7 +73,7 @@ class AbstractCar:
             pygame.draw.line(win, (0, 255, 0), self.center, position, 1)
             pygame.draw.circle(win, (255, 0, 0), position, 3)
 
-    def check_radar(self,degree, game_map):
+    def check_radar(self, degree, game_map):
         length = 0
         x = int(self.center[0] + math.cos(math.radians(360 - (self.angle + degree))) * length)
         y = int(self.center[1] + math.sin(math.radians(360 - (self.angle + degree))) * length)
@@ -106,7 +88,6 @@ class AbstractCar:
         dist = int(math.sqrt(math.pow(x - self.center[0], 2) + math.pow(y - self.center[1], 2)))
         self.radars.append([(x, y), dist])
 
-
     def get_data(self):
         radars = self.radars
         return_values = [0, 0, 0, 0, 0]
@@ -118,26 +99,25 @@ class AbstractCar:
     def get_reward(self):
         return self.distance / CAR_SIZE_X / 2
 
-
     def update(self, game_map):
         if not self.speed_set:
             self.speed = 20
             self.speed_set = True
 
-        self.rotated_sprite = self.rotate_center(self.sprite, self.angle)
+        self.rotated_sprite = rotate_center(self.sprite, self.angle)
         self.position[0] += math.cos(math.radians(360 - self.angle)) * self.speed
         self.position[0] = max(self.position[0], 20)
         self.position[0] = min(self.position[0], WIDTH - 120)
 
         length = 0.5 * CAR_SIZE_X
-        left_top = [self.center[0] + math.cos(math.radians(360 - (self.angle + 30))) * length,
-                    self.center[1] + math.sin(math.radians(360 - (self.angle + 30))) * length]
-        right_top = [self.center[0] + math.cos(math.radians(360 - (self.angle + 150))) * length,
-                     self.center[1] + math.sin(math.radians(360 - (self.angle + 150))) * length]
-        left_bottom = [self.center[0] + math.cos(math.radians(360 - (self.angle + 210))) * length,
-                       self.center[1] + math.sin(math.radians(360 - (self.angle + 210))) * length]
-        right_bottom = [self.center[0] + math.cos(math.radians(360 - (self.angle + 330))) * length,
-                        self.center[1] + math.sin(math.radians(360 - (self.angle + 330))) * length]
+        left_top = [self.center[0] + math.cos(math.radians((self.angle + 30))) * length,
+                    self.center[1] + math.sin(math.radians((self.angle + 30))) * length]
+        right_top = [self.center[0] + math.cos(math.radians((self.angle + 150))) * length,
+                     self.center[1] + math.sin(math.radians((self.angle + 150))) * length]
+        left_bottom = [self.center[0] + math.cos(math.radians((self.angle + 210))) * length,
+                       self.center[1] + math.sin(math.radians((self.angle + 210))) * length]
+        right_bottom = [self.center[0] + math.cos(math.radians((self.angle + 330))) * length,
+                        self.center[1] + math.sin(math.radians((self.angle + 330))) * length]
         self.corners = [left_top, right_top, left_bottom, right_bottom]
 
         self.distance += self.speed
@@ -166,21 +146,32 @@ class AbstractCar:
                 self.alive = False
                 break
 
-    def rotate_center(self, image, angle):
-        # Rotate The Rectangle
-        rectangle = image.get_rect()
-        rotated_image = pygame.transform.rotate(image, angle)
-        rotated_rectangle = rectangle.copy()
-        rotated_rectangle.center = rotated_image.get_rect().center
-        rotated_image = rotated_image.subsurface(rotated_rectangle).copy()
-        return rotated_image
+    def finish_line(self, finish_mask):
+        offset = (int(self.position[0]) - FINISH_LINE_POS[0], int(self.position[1]) - FINISH_LINE_POS[1])
+
+        overlap = finish_mask.overlap(self.mask, offset)
+
+        if overlap:
+            return True
+        else:
+            return False
 
 
+def rotate_center(image, angle):
+    # Rotate The Rectangle
+    rectangle = image.get_rect()
+    rotated_image = pygame.transform.rotate(image, angle)
+    rotated_rectangle = rectangle.copy()
+    rotated_rectangle.center = rotated_image.get_rect().center
+    rotated_image = rotated_image.subsurface(rotated_rectangle).copy()
+    return rotated_image
 
-def run_simulation(genomes, config):
+
+def run_simulation(genomes, config, counter=0):
     # Empty Collections For Nets and Cars
     nets = []
     cars = []
+    global INDEX_TRACK
 
     # Initialize PyGame And The Display
     pygame.init()
@@ -197,15 +188,16 @@ def run_simulation(genomes, config):
     # Clock Settings
     # Font Settings & Loading Map
     clock = pygame.time.Clock()
-    generation_font = pygame.font.SysFont("Arial", 30)
-    alive_font = pygame.font.SysFont("Arial", 20)
-    game_map = pygame.image.load('./imgs/track2.png').convert()  # Convert Speeds Up A Lot
+    game_map = pygame.image.load(TRACKS[INDEX_TRACK]).convert()
 
 
     # Simple Counter To Roughly Limit Time (Not Good Practice)
-    counter = 0
+
+    print(counter, 'started.')
 
     while True:
+        finished = False
+
         # Exit On Quit Event
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -237,23 +229,57 @@ def run_simulation(genomes, config):
         if still_alive == 0:
             break
 
-        counter += 1
-        if counter == 30 * 40:  # Stop After About 20 Seconds
-            break
-
         # Draw Map And All Cars That Are Alive
         screen.blit(game_map, (0, 0))
+        screen.blit(FINISH_LINE, (FINISH_LINE_POS[0], FINISH_LINE_POS[1]))
+
         for car in cars:
             if car.isAlive():
                 car.draw(screen)
+                if car.finish_line(FINISH_LINE_MASK):
+                    print("you won")
+                    INDEX_TRACK += 1
+                    finished = True
 
         # Display Info
-
+        if finished:
+            print(counter, 'changed')
+            break
 
         pygame.display.flip()
         clock.tick(60)  # 60 FPS
 
+    return counter
 
+
+def get_latest_checkpoint(prefix="neat-checkpoint"):
+    files = os.listdir('.')
+    checkpoints = []
+
+    pattern = re.compile(rf"{prefix}(\d+)$")
+
+    for f in files:
+        match = pattern.match(f)
+        if match:
+            number = int(match.group(1))
+            checkpoints.append((number, f))
+
+    if not checkpoints:
+        return None
+
+    checkpoints.sort(key=lambda x: x[0])
+    return checkpoints[-1][1]
+
+
+def remove_checkpoint(prefix="neat-checkpoint"):
+    files = os.listdir('.')
+
+    pattern = re.compile(rf"{prefix}(\d+)$")
+
+    for f in files:
+        match = pattern.match(f)
+        if match:
+            os.remove(f)
 
 
 if __name__ == "__main__":
@@ -264,11 +290,23 @@ if __name__ == "__main__":
                                 neat.DefaultStagnation,
                                 config_path)
 
-    # Create Population And Add Reporters
-    population = neat.Population(config)
+    checkpoint = get_latest_checkpoint()
+
+    INDEX_TRACK = 0
+
+    if checkpoint:
+        population = neat.Checkpointer.restore_checkpoint(checkpoint)
+        population.generation = 0
+        population.reporters = neat.reporting.ReporterSet()
+        remove_checkpoint()
+    else:
+        population = neat.Population(config)
+
     population.add_reporter(neat.StdOutReporter(True))
     stats = neat.StatisticsReporter()
     population.add_reporter(stats)
 
-    # Run Simulation For A Maximum of 1000 Generations
+    population.add_reporter(neat.Checkpointer(generation_interval=5,
+                                              filename_prefix="neat-checkpoint"))
+
     population.run(run_simulation, 9000)
